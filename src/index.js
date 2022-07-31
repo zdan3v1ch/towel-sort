b720edb4b0ae346dd24d9cbc644ec93abaeb4509
Anatoli_Z <anatoli.zdanevitch@gmail.com>
2022-07-31 20:00:15 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined) {
+    return []
+  }
+  for (i =0; i< matrix.length; i++){
+    if (i % 2 != 0) {
+      matrix[i] = matrix[i].reverse()
+    }
+  }
+  return matrix.flat();
 }
+
+
+
+
+
